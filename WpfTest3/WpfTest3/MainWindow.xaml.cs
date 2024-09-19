7b3c618193e4c76cb08afc2d42e5c8f58beb3bee
SaykoES <saykoes@mer.ci.nsu.ru>
2024-09-19 10:43:30 +0700

    Lab3

---
+++ WpfTest3/WpfTest3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTest3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void rbColor_Click(object sender, RoutedEventArgs e)
+        {
+            var radio = (sender as RadioButton);
+            string radioName = radio.Name;
+            
+            if (radioName == "rbRed")
+                mainMenu.Background = Brushes.Red;
+            if (radioName == "rbYellow")
+                mainMenu.Background = Brushes.Yellow;
+
+
+        }
+    }
+}
